fca5bfafb614d54c90bf0f02930d03ba178d5df0
Deepak Goyal <30751816+Deepakgoyal-iOS@users.noreply.github.com>
2025-02-13 22:53:20 +0530

    Update UXPageTabCollectionViewCell.swift

--- Sources/UXPagerView/Architecture/classes/UXPageTabCollectionViewCell.swift
+++ Sources/UXPagerView/Architecture/classes/UXPageTabCollectionViewCell.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class UXPageTabCollectionViewCell: UICollectionViewCell {
+public class UXPageTabCollectionViewCell: UICollectionViewCell {
 
     @IBOutlet weak var titleLbl: UILabel!
     @IBOutlet weak var badgeCountLbl: UILabel!
